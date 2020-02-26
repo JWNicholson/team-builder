9beb304e32283c8ef783a1d3cb43c70e4b729634
JW Nicholson <john.nicholson812@gmail.com>
2020-02-26 18:48:08 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import logo from './logo.svg';
 import './App.css';
+import Members from "./components/Members";
+import MemberForm from "./components/MemberForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      boo
+     <MemberForm />
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
-
+import ReactDOM from "react-dom"
 import Members from "./components/Members";
-
 import MemberForm from "./components/MemberForm";
-
 import "./index.css";
 
 function App() {
-  const [members, setMembers] = useState([
-    {
-      name: "Bob",
-      email: "Evans",
-      role: "busboy",
-    }
-  ]);
+   const [members, setMembers] = useState([
+     {
+       name: "Bob",
+       email: "Evans",
+       role: "busboy",
+     }
+   ]);
 
-  const addNewMember = member => {
-    const newMember = {
-      name: member.name,
-      email: member.email,
-      role: member.role
-    };
-    setMembers([...members, newMember]);
-  };
+   const addNewMember = member => {
+     const newMember = {
+       name: member.name,
+       email: member.email,
+       role: member.role
+     };
+     setMembers([...members, newMember]);
+   };
 
-  return (
+   return (
    
-    <div className="App">
-      <h1>Members</h1>
-      {/* pass a function down as a prop */}
-      <MemberForm addNewMember={addNewMember} />
-      <h2>Members</h2>
-      <Members members={members} />
-    </div>
-  );
+     <div className="App">
+       <h1>Members List</h1>
+       {/* pass a function down as a prop */}
+       <MemberForm addNewMember={addNewMember} />
+       <h2>Members:</h2>
+       <Members members={members} />
+     </div>
+   );
 }
 
 const rootElement = document.getElementById("root");
